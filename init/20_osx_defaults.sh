e1c148e666b3d9d686fce844a0675840e636599a
Ezbon Jacob <ezjacob@gmail.com>
2014-11-21 14:56:18 -0500

    added some OS X defaults and terminal theme files

--- init/20_osx_defaults.sh
+++ init/20_osx_defaults.sh
@@ -20,6 +20,9 @@ defaults write com.apple.BezelServices kDimTime -int 300
 #Set Clock
 defaults write com.apple.menuextra.clock DateFormat -string 'EEE MMM d h:mm:ss a'
 
+#Enable Dark mode
+sudo defaults write /Library/Preferences/.GlobalPreferences.plist _HIEnableThemeSwitchHotKey -bool true
+
 ###############################################################################
 # Screen                                                                      #
 ###############################################################################
@@ -86,6 +89,16 @@ defaults write com.apple.mail DisableInlineAttachmentViewing -bool true
 # Only use UTF-8 in Terminal.app
 defaults write com.apple.terminal StringEncodings -array 4
 
+# Use a modified version of the Solarized Dark theme by default in Terminal.app
+TERM_PROFILE='Homebrew';
+CURRENT_PROFILE="$(defaults read com.apple.terminal 'Default Window Settings')";
+if [ "${CURRENT_PROFILE}" != "${TERM_PROFILE}" ]; then
+    open "${DOTFILES}/conf/${TERM_PROFILE}.terminal";
+    sleep 1; # Wait a bit to make sure the theme is loaded
+    defaults write com.apple.terminal 'Default Window Settings' -string "${TERM_PROFILE}";
+    defaults write com.apple.terminal 'Startup Window Settings' -string "${TERM_PROFILE}";
+fi;
+
 
 ###############################################################################
 # Time Machine                                                                #
@@ -101,7 +114,7 @@ defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 
 for app in "cfprefsd" \
     "Dock" "Finder" "Mail""SystemUIServer" \
-    "Terminal"; do
+    ; do
     killall "${app}" > /dev/null 2>&1
 done
 echo "Done. Note that some of these changes require a logout/restart to take effect."
