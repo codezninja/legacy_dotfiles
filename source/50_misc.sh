fbae180bfe4776cf246a3b2f712c3ad9fd3c037c
Ezbon Jacob <ezjacob@gmail.com>
2015-06-16 09:57:13 -0400

    updating misc items 	- added docker-enter 	- added function docker-ip 	- apache module defate for osx enabled 	- sublime text packages 	- tmux mapping

--- source/50_misc.sh
+++ source/50_misc.sh
@@ -19,3 +19,8 @@ function titlebar() {
 if [[ -e ~/.ssh/known_hosts ]]; then
   complete -o default -W "$(cat ~/.ssh/known_hosts | sed 's/[, ].*//' | sort | uniq | grep -v '[0-9]')" ssh scp sftp
 fi
+
+# Docker
+docker-ip() {
+  boot2docker ip 2> /dev/null
+}
