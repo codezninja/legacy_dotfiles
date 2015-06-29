fb6d31f14f00e64990a0014c36cba924a484c453
Ezbon Jacob <ezbon.jacob@manheim.com>
2015-06-29 11:00:16 -0400

    adding ENV $JAVA_HOME

--- source/50_osx.sh
+++ source/50_osx.sh
@@ -4,6 +4,7 @@ is_osx || return 1
 # APPLE, Y U PUT /usr/bin B4 /usr/local/bin?!
 PATH="/usr/local/bin:$(path_remove /usr/local/bin)"
 export PATH
+export JAVA_HOME=$(/usr/libexec/java_home)
 
 # Trim new lines and copy to clipboard
 alias c="tr -d '\n' | pbcopy"
