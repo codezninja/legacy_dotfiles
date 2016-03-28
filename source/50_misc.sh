f5669f4ee6e00218f57adb0c2f2ff1e58bafd50f
Ezbon Jacob <ezbon.jacob@manheim.com>
2016-03-28 10:11:55 -0400

    - added jsoncompare.py - added tmux wrapper tm - added alias tmn for terminal-notifier

--- source/50_misc.sh
+++ source/50_misc.sh
@@ -20,7 +20,5 @@ if [[ -e ~/.ssh/known_hosts ]]; then
   complete -o default -W "$(cat ~/.ssh/known_hosts | sed 's/[, ].*//' | sort | uniq | grep -v '[0-9]')" ssh scp sftp
 fi
 
-# Docker
-docker-ip() {
-  boot2docker ip 2> /dev/null
-}
+#Terminal Notifier
+alias tmn="terminal-notifier -message"
