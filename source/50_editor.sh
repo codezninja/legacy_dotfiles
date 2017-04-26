6e21c2f76b2599309cacb1dd9a7572c91a3e437c
Ezbon Jacob <ezbon.jacob@manheim.com>
2017-04-26 08:07:46 -0400

    updated alias sublp

--- source/50_editor.sh
+++ source/50_editor.sh
@@ -7,8 +7,31 @@
   export EDITOR='vim'
 # fi
 
+function openInSublime() {
+  if [ -f *.sublime-project ]; then
+    subl --project *.sublime-project;
+  else
+    sublime_project_file_template="$(cat <<EOF
+{
+  "folders":
+  [
+    {
+      "path": "."
+    }
+  ]
+}
+EOF
+)"
+    sublime_project_file_name="${PWD##*/}.sublime-project"
+    echo "No Sublime Project Found ... Creating $sublime_project_file_name";
+    echo "$sublime_project_file_template" >> $sublime_project_file_name
+
+    subl --project *.sublime-project;
+  fi
+}
+
 export VISUAL="$EDITOR"
 alias q="$EDITOR"
 alias qv="q $DOTFILES/link/.{,g}vimrc +'cd $DOTFILES'"
 alias qs="q $DOTFILES"
-alias sublp="if [ -f *.sublime-project ]; then subl --project *.sublime-project; else echo 'No Sublime Project Found'; fi"
+alias sublp=openInSublime
