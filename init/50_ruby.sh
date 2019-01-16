eff1542245db588a95305d4ba45d87093d039409
Ezbon Jacob <ezjacob@gmail.com>
2019-01-15 23:08:46 -0500

    added jruby and ruby

--- init/50_ruby.sh
+++ init/50_ruby.sh
@@ -3,13 +3,14 @@ source $DOTFILES/source/50_ruby.sh
 
 # Install Ruby.
 if [[ "$(type -P rbenv)" ]]; then
-  versions=(2.3.1) # 2.0.0-p576 1.9.3-p547)
+  versions=(2.3.1 2.5.1 jruby-9.1.1.0)
 
   rubies=($(setdiff "${versions[*]}" "$(rbenv whence ruby)"))
   if (( ${#rubies[@]} > 0 )); then
     e_header "Installing Ruby versions: ${rubies[*]}"
     for r in "${rubies[@]}"; do
       rbenv install "$r"
+      rbenv exec gem install bundler
       [[ "$r" == "${versions[0]}" ]] && rbenv global "$r"
     done
   fi
--- link/.bash_profile
+++ link/.bash_profile
@@ -5,3 +5,4 @@ complete -C aws_completer aws
 export PATH=$PATH:~/go/bin
 export PATH="$HOME/.jenv/bin:$PATH"
 eval "$(jenv init -)"
+export PATH="/usr/local/opt/openssl/bin:$PATH"
